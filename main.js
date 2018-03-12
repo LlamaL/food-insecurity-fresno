f3e6eb07073aa33f04a3751583d4be1267efb31f
TianCW <mstiencw@gmail.com>
2018-03-12 01:03:36 -0500

    finish data binding

--- main.js
+++ main.js
@@ -5,8 +5,33 @@ L.tileLayer('https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey
 })
   .addTo(map);
 
-map.setView([36.73, -119.78], 12);
+map.setView([36.78, -119.78], 12);
 
-for (var i = 0; i < store.length; i++) {
-	array[i]
+for (var num = 0; num < store.length; num++) {
+	var stores = store[num];
+	var store_lat = stores["Lat"];
+	var store_lng = stores["Lng"];
+	var store_name = stores["name"];
+	var marker = L.circleMarker([store_lat, store_lng], {
+		stroke: false,
+		radius: 6,
+		fillOpacity: 0.6,
+	})
+	   .addTo(map)
+		 .bindTooltip(store_name);
+}
+
+for (var num = 0; num < liquor.length; num++) {
+	var liquors = liquor[num];
+	var liquor_lat = liquors["Lat"];
+	var liquor_lng = liquors["Lng"];
+	var liquor_name = liquors["name"];
+	var marker = L.circleMarker([liquor_lat, liquor_lng], {
+		stroke: false,
+		radius: 6,
+		fillOpacity: 0.6,
+		fillColor: "red",
+	})
+	   .addTo(map)
+		 .bindTooltip(liquor_name);
 }

13ffa8b3440f4e4889f7942c79dbde7b97af8e77
TianCW <mstiencw@gmail.com>
2018-03-29 19:14:18 -0700

    Organized folder

---
+++ fresno-meth/data/ca_meth.R
@@ -0,0 +1,16 @@
+library(readr)
+library(dplyr)
+
+#Import and Load Data
+national_meth <- read_csv("meth_seizures_90_17.csv")
+
+#filter for CA
+ca_meth <- national_meth %>%
+  filter(State == "CA") %>%
+  group_by(State)
+  
+#View structure of the data
+glimpse(ca_meth)
+
+#Sum for different years
+ca_meth_year <- aggregate(ca_meth$`Nt Wt`, list(year = ca_meth$`Seize Year`), sum)
